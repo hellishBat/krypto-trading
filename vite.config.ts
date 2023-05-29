bcaa2dddb8b92675c57262a3dc28eeb29888fa2f
Valentine Samoylov <valentine.samoylov@gmail.com>
2023-05-29 23:54:30 +0300

    Fixed file import.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-// Features
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
@@ -38,7 +37,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@/': `${path.resolve(__dirname, 'src')}/`,
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 })
